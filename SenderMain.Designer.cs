cca0835a4e02a3de3b22572b94da9ec5c0199047
yyliujc <27572161@qq.com>
2019-12-18 11:06:04 +0800

    ui

--- SenderMain.Designer.cs
+++ SenderMain.Designer.cs
@@ -37,53 +37,55 @@
             // 
             // txtEmailList
             // 
-            this.txtEmailList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtEmailList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
             this.txtEmailList.Location = new System.Drawing.Point(13, 13);
             this.txtEmailList.Multiline = true;
             this.txtEmailList.Name = "txtEmailList";
             this.txtEmailList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtEmailList.Size = new System.Drawing.Size(178, 341);
+            this.txtEmailList.Size = new System.Drawing.Size(178, 413);
             this.txtEmailList.TabIndex = 0;
             this.txtEmailList.Text = "邮箱列表";
             // 
             // txtTitle
             // 
-            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.txtTitle.Location = new System.Drawing.Point(198, 13);
             this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(335, 21);
+            this.txtTitle.Size = new System.Drawing.Size(400, 21);
             this.txtTitle.TabIndex = 1;
             this.txtTitle.Text = "邮件标题";
             // 
             // txtContent
             // 
-            this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.txtContent.Location = new System.Drawing.Point(198, 41);
             this.txtContent.Multiline = true;
             this.txtContent.Name = "txtContent";
-            this.txtContent.Size = new System.Drawing.Size(403, 138);
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(468, 138);
             this.txtContent.TabIndex = 2;
             this.txtContent.Text = "邮件内容";
             // 
             // txtLog
             // 
-            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.txtLog.Location = new System.Drawing.Point(198, 186);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
-            this.txtLog.Size = new System.Drawing.Size(403, 168);
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(468, 240);
             this.txtLog.TabIndex = 3;
             this.txtLog.Text = "输出日志";
             // 
             // btnSend
             // 
             this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSend.Location = new System.Drawing.Point(540, 13);
+            this.btnSend.Location = new System.Drawing.Point(605, 13);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(61, 23);
             this.btnSend.TabIndex = 4;
@@ -91,17 +93,17 @@
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
-            // Form1
+            // SenderMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(613, 366);
+            this.ClientSize = new System.Drawing.Size(678, 438);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.txtContent);
             this.Controls.Add(this.txtTitle);
             this.Controls.Add(this.txtEmailList);
-            this.Name = "Form1";
+            this.Name = "SenderMain";
             this.Text = "简易邮件群发";
             this.ResumeLayout(false);
             this.PerformLayout();
